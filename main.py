9312268a64f445f2683e4f72a38952cd6fabbcd4
Rotten-Manuel <rottenimugi@gmail.com>
2025-10-27 13:04:13 -0500

    0.0.4

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
 
     running = True
     while running:
-        pygame.Surface.fill(screen, (0,0,1))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.fill("black")
+        screen.fill((0,0,0))
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-    pygame.QUIT()
 
+        # shit is happening  here
+        dt = clock.tick(60) / 1000
+
+    return
 
 if __name__ == "__main__":
     main()
